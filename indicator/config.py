70f8018de97133103dbb7cc2747f8c368ef00276
Ashley Anderson <aganders3@gmail.com>
2018-08-27 15:20:18 -0500

    Change db location config

--- indicator/config.py
+++ indicator/config.py
@@ -3,7 +3,8 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config():
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    default_db_path = os.path.join(os.environ.get('DATABASE_BASEDIR'), 'indicator.db')
+    SQLALCHEMY_DATABASE_URI =  default_db_path or \
         'sqlite:///' + os.path.join(basedir, 'indicator.db')
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
